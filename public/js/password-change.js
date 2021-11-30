0520dfb60b587ba74d5f372cab0def2b9755aee1
ste <git@grundell.co.uk>
2021-11-30 15:03:53 +0000

    password change

---
+++ public/js/password-change.js
@@ -0,0 +1,25 @@
+import { formValidation } from "./main.js";
+import { changePassword, getToken } from "./api.js";
+import { config } from "./config.js";
+
+if (!getToken().token) {
+  // Need to be logged in to change the password.
+  sessionStorage.setItem("errorMessage", "You need to sign in before changing your password");
+  location.href = config.pages.login;
+}
+
+$(function () {
+
+  const form = document.getElementById("passwordForm");
+  const currentPassword = document.getElementById("currentPassword");
+  const password = document.getElementById("password");
+  const passwordConfirm = document.getElementById("passwordConfirm");
+
+  formValidation(form, () => changePassword(currentPassword, password));
+
+  // Ensure the passwordConfirm matches the password.
+  $(password).on("input", function () {
+    passwordConfirm.setAttribute("pattern", password.value);
+  });
+
+});
